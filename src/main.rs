05d4f0d54d9aa36d7e8592a79d8146b06582edc9
Stefano <stdemicheli@protonmail.com>
2023-01-02 04:39:32 +0100

    Adds configuration file.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::net::TcpListener;
 use rust_web_server::startup::run;
+use rust_web_server::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(TcpListener::bind("127.0.0.1:8000").unwrap())?.await
+    let configuration = get_configuration().expect("Faild to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
