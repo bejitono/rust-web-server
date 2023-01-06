887e86213bd3560f27b8dfd6133436a4741ac1f8
Stefano <stdemicheli@protonmail.com>
2023-01-06 16:07:56 +0100

    Initializes env logger.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 use sqlx::PgPool;
+use env_logger::Env;
 use rust_web_server::startup::run;
 use rust_web_server::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Faild to read configuration.");
     let connection_pool = PgPool::connect(
             &configuration.database.connection_string()
