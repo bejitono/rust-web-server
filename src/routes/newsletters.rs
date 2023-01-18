b66ea1b4018c664f9f5ed92b6f1afc070eb6cd09
Stefano <stdemicheli@protonmail.com>
2023-01-18 07:12:32 +0100

    Implements error types.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,7 @@
-use actix_web::{HttpResponse, web};
+use actix_web::{HttpResponse, web, ResponseError};
+use reqwest::StatusCode;
 use sqlx::PgPool;
+use crate::routes::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -16,8 +18,12 @@ struct ConfirmedSubscriber {
     email: String
 }
 
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn publish_newsletter(
+    _body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
@@ -36,4 +42,24 @@ async fn get_confirmed_subscribers(
     .await?;
     
     Ok(rows)
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
