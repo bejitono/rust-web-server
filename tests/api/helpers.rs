e63ae6e9d3a846e845a49a37408d7f42a06abc1b
Stefano <stdemicheli@protonmail.com>
2023-01-20 09:44:37 +0100

    Updates store creds test.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -2,7 +2,8 @@ use once_cell::sync::Lazy;
 use rust_web_server::configuration::{get_configuration, DatabaseSettings};
 use rust_web_server::startup::{get_connection_pool, Application};
 use rust_web_server::telemetry::{get_subscriber, init_subscriber};
-use sha3::Digest;
+use argon2::password_hash::SaltString;
+use argon2::{Argon2, PasswordHasher};
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
@@ -153,8 +154,11 @@ impl TestUser {
     }
 
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::default()
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash) VALUES ($1, $2, $3)",
             self.user_id,
