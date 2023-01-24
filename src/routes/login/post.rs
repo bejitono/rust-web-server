29ad425c223a42a82f1c28c19f3c77e4ba764db1
Stefano <stdemicheli@protonmail.com>
2023-01-24 06:07:13 +0100

    Refactors using cookie msg store.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -8,6 +8,7 @@ use actix_web::http::StatusCode;
 use actix_web::web;
 use actix_web::HttpResponse;
 use actix_web::ResponseError;
+use actix_web_flash_messages::FlashMessage;
 use hmac::{Hmac, Mac};
 use secrecy::Secret;
 use sqlx::PgPool;
@@ -48,10 +49,9 @@ pub async fn login(
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
+            FlashMessage::error(e.to_string()).send();
             let response = HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/login"))
-                // Set a cookie
-                .cookie(Cookie::new("_flash", e.to_string()))
                 .finish();
             Err(InternalError::from_response(e, response))
         }
