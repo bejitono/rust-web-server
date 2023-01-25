6d82820f6cf7566e62561d28a446acf1e6e19802
Stefano <stdemicheli@protonmail.com>
2023-01-25 06:15:31 +0100

    Replaces actix session with typed session.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,7 @@
 use crate::authentication::AuthError;
 use crate::authentication::{validate_credentials, Credentials};
 use crate::routes::error_chain_fmt;
-use actix_session::Session;
+use crate::session_state::TypedSession;
 use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::web;
@@ -27,7 +27,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -41,7 +41,7 @@ pub async fn login(
             // Renew session in order to prevent attacks where the browser is seeded with a known session token before they log in.
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
