eefce9141fdf9913ebc93b1d8bfdd3143ca27cd6
Stefano <stdemicheli@protonmail.com>
2023-01-25 05:58:59 +0100

    Removes redundant code.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -4,12 +4,9 @@ use crate::routes::error_chain_fmt;
 use actix_session::Session;
 use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
-use actix_web::http::StatusCode;
 use actix_web::web;
 use actix_web::HttpResponse;
-use actix_web::ResponseError;
 use actix_web_flash_messages::FlashMessage;
-use hmac::{Hmac, Mac};
 use secrecy::Secret;
 use sqlx::PgPool;
 
@@ -75,27 +72,6 @@ pub enum LoginError {
 impl std::fmt::Debug for LoginError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
-    }
-}
-
-impl ResponseError for LoginError {
-    fn error_response(&self) -> HttpResponse {
-        let query_string = format!("error={}", urlencoding::Encoded::new(self.to_string()));
-        let secret: &[u8] = todo!();
-        let hmac_tag = {
-            let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret).unwrap();
-            mac.update(query_string.as_bytes());
-            mac.finalize().into_bytes()
-        };
-        HttpResponse::build(self.status_code())
-            // Appending the hexadecimal representation of the HMAC tag to the
-            // query string as an additional query parameter.
-            .insert_header((LOCATION, format!("/login?{query_string}&tag={hmac_tag:x}")))
-            .finish()
-    }
-
-    fn status_code(&self) -> StatusCode {
-        StatusCode::SEE_OTHER
     }
 }
 
