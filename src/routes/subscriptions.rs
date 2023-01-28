f40bfdf5ce67b4d1ee3f7bf7644beeba50652694
Stefano <stdemicheli@protonmail.com>
2023-01-28 08:05:13 +0100

    Fixes formatting issues.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -121,9 +121,8 @@ pub async fn send_confirmation_email(
     base_url: &str,
     subscription_token: &str,
 ) -> Result<(), reqwest::Error> {
-    let confirmation_link = format!(
-        "{base_url}/subscriptions/confirm?subscription_token={subscription_token}"
-    );
+    let confirmation_link =
+        format!("{base_url}/subscriptions/confirm?subscription_token={subscription_token}");
     let plain_body = format!(
         "Welcome to our newsletter!<br />\
         Click <a href=\"{confirmation_link}\">here</a> to confirm your subscription."
